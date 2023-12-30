66da812a0b3e1161df905ba7492ca300cc1979f7
Ulguim <williamulguim2014@gmail.com>
2023-12-30 20:20:11 -0300

    :art: Feat: DUB-18 update uploadHook

--- src/helper/imageUploadHandler.ts
+++ src/helper/imageUploadHandler.ts
@@ -7,7 +7,7 @@ const useFileUpload = () => {
   const imageUpload = async (file: ImageType) => {
     const currentTimeStamp = new Date().getTime();
     const { data, error } = await supabase.storage
-      .from("dubinhasDev")
+      .from(process.env.NEXT_PUBLIC_SUPABASE_BUCKET)
       .upload(`public/${currentTimeStamp}`, file as File, {
         cacheControl: "3600",
         upsert: false,
